9ea3e8ff53041e9bcba60334368a7df54e87cbfe
rowtea <hluystikov.maxim@gmail.com>
2020-04-22 22:28:21 +0300

    добавил вх, доработал то что ты сказал..и

--- Admin_Tools.lua
+++ Admin_Tools.lua
@@ -12,6 +12,17 @@ u8 = encoding.UTF8
 -- Инклуды --
 
 
+whVisible = "all"
+optionsCommand = "settingswh"
+KEY = VK_F5
+defaultState = false
+
+
+local ffi = require "ffi"
+local getBonePosition = ffi.cast("int (__thiscall*)(void*, float*, int, bool)", 0x5E4280)
+require "lib.moonloader"
+local mem = require "memory"
+
 -- Переменные --
 local activate = false
 imgui.Process = false
@@ -93,7 +104,6 @@ function apply_custom_style()
     do
     
     apply_custom_style()
-    
     
     local slider_float = imgui.ImFloat(0.0)
     local clear_color = imgui.ImVec4(0.45, 0.55, 0.60, 1.00)
@@ -260,6 +270,13 @@ function main()
 
     -- Команды --
     sampRegisterChatCommand("amenu", cmd_amenu)
+    sampRegisterChatCommand(optionsCommand, function(param)
+		if param == "bones" then whVisible = param; nameTagOff()
+		elseif param == "names" or param == "all" then whVisible = param if not nameTag then nameTagOn() end
+		else sampAddChatMessage("{ff0000}[Admin Tools]{FFFFFF} Введите корректный режим: names/bones/all", 0xFF4444FF) end
+	end)
+	while not sampIsLocalPlayerSpawned() do wait(100) end
+	if defaultState and not nameTag then nameTagOn() end
     -- Команды --
 
     wait(0)
@@ -270,6 +287,57 @@ function main()
 
         imgui.Process = show_admin_menu.v or show_admin_info.v or show_admin_tp.v
 
+        if wasKeyPressed(KEY) then; 
+            if defaultState then
+                sampAddChatMessage("{ff0000}[Admin Tools]{FFFFFF} WallHack {ff0000}выключен{ffffff}", 0xFFFFFFFF)
+                defaultState = false; 
+				nameTagOff(); 
+				while isKeyDown(KEY) do wait(100) end 
+			else
+                defaultState = true;
+                sampAddChatMessage("{ff0000}[Admin Tools]{FFFFFF} WallHack {20ed15}включен{ffffff}", 0xFFFFFFFF)
+				if whVisible ~= "bones" and not nameTag then nameTagOn() end
+				while isKeyDown(KEY) do wait(100) end 
+			end 
+		end
+		if defaultState and whVisible ~= "names" then
+			if not isPauseMenuActive() and not isKeyDown(VK_F8) then
+				for i = 0, sampGetMaxPlayerId() do
+				if sampIsPlayerConnected(i) then
+					local result, cped = sampGetCharHandleBySampPlayerId(i)
+					local color = sampGetPlayerColor(i)
+					local aa, rr, gg, bb = explode_argb(color)
+					local color = join_argb(255, rr, gg, bb)
+					if result then
+						if doesCharExist(cped) and isCharOnScreen(cped) then
+							local t = {3, 4, 5, 51, 52, 41, 42, 31, 32, 33, 21, 22, 23, 2}
+							for v = 1, #t do
+								pos1X, pos1Y, pos1Z = getBodyPartCoordinates(t[v], cped)
+								pos2X, pos2Y, pos2Z = getBodyPartCoordinates(t[v] + 1, cped)
+								pos1, pos2 = convert3DCoordsToScreen(pos1X, pos1Y, pos1Z)
+								pos3, pos4 = convert3DCoordsToScreen(pos2X, pos2Y, pos2Z)
+								renderDrawLine(pos1, pos2, pos3, pos4, 1, color)
+							end
+							for v = 4, 5 do
+								pos2X, pos2Y, pos2Z = getBodyPartCoordinates(v * 10 + 1, cped)
+								pos3, pos4 = convert3DCoordsToScreen(pos2X, pos2Y, pos2Z)
+								renderDrawLine(pos1, pos2, pos3, pos4, 1, color)
+							end
+							local t = {53, 43, 24, 34, 6}
+							for v = 1, #t do
+								posX, posY, posZ = getBodyPartCoordinates(t[v], cped)
+								pos1, pos2 = convert3DCoordsToScreen(posX, posY, posZ)
+							end
+						end
+					end
+				end
+			end
+			else
+				nameTagOff()
+				while isPauseMenuActive() or isKeyDown(VK_F8) do wait(0) end
+				nameTagOn()
+			end
+		end
         if wasKeyPressed(key.VK_P) then
             local chatin = sampIsChatInputActive()
             if chatin == false then show_admin_menu.v = not show_admin_menu.v end
@@ -308,17 +376,13 @@ function main()
             end
             if forma == true then
                 if formacmd == "inftime" then
-                    lua_thread.create(function()
-                        sampSendChat("/pm "..formaid2.." Если вы не согласны с получением наказания напишите жалобу на форум...")
-                        wait(500)
-                        sampSendChat("/pm "..formaid2.." Наш Форум: forum.samp-states.ru")
+                        sampSendChat("/pm "..formaid.." Если вы не согласны с наказанием напишите жалобу forum.samp-states.ru")
                         wait(500)
                         sampSendChat("/"..formacmd.." "..formaid2.." • "..formaadm)
                         lua_thread.create(function() wait(1000) sampSendChat("/a [FORMA] +") end)
                         forma = false
                         formacmd = nil
                         formaid = nil
-                    end)
                 else
                     sampSendChat("/"..formacmd.." "..formaid2.." • "..formaadm)
                     lua_thread.create(function() wait(1000) sampSendChat("/a [FORMA] +") end)
@@ -329,20 +393,16 @@ function main()
 			end
             if formc == true then
                 if formacmd == "jail" or formacmd == "ban" then
-                    lua_thread.create(function()
-                        sampSendChat("/pm "..formaid3.." Если вы не согласны с получением наказания напишите жалобу на форум...")
-                        wait(500)
-                        sampSendChat("/pm "..formaid3.." Наш Форум: forum.samp-states.ru")
-                        wait(500)
-                    end)
-                end
+                sampSendChat("/pm "..formaid.." Если вы не согласны с наказанием напишите жалобу forum.samp-states.ru")
+                wait(1100)
                 sampSendChat("/"..formacmd.." "..formaid3.." "..formatime3.." "..formareason3.." • "..formaadm)
 				lua_thread.create(function() wait(1000) sampSendChat("/a [FORMA] +") end)
                 formc = false
                 formacmd = nil
 				formaid3 = nil
 				formatime3 = nil
-				formareason3 = nil
+                formareason3 = nil
+                end
             end
         end
     end
@@ -435,3 +495,45 @@ if formb == true then
          end
     end
 end
+
+function getBodyPartCoordinates(id, handle)
+    local pedptr = getCharPointer(handle)
+    local vec = ffi.new("float[3]")
+    getBonePosition(ffi.cast("void*", pedptr), vec, id, true)
+    return vec[0], vec[1], vec[2]
+  end
+  
+  function nameTagOn()
+      local pStSet = sampGetServerSettingsPtr();
+      NTdist = mem.getfloat(pStSet + 39)
+      NTwalls = mem.getint8(pStSet + 47)
+      NTshow = mem.getint8(pStSet + 56)
+      mem.setfloat(pStSet + 39, 1488.0)
+      mem.setint8(pStSet + 47, 0)
+      mem.setint8(pStSet + 56, 1)
+      nameTag = true
+  end
+  
+  function nameTagOff()
+      local pStSet = sampGetServerSettingsPtr();
+      mem.setfloat(pStSet + 39, NTdist)
+      mem.setint8(pStSet + 47, NTwalls)
+      mem.setint8(pStSet + 56, NTshow)
+      nameTag = false
+  end
+  
+  function join_argb(a, r, g, b)
+    local argb = b  -- b
+    argb = bit.bor(argb, bit.lshift(g, 8))  -- g
+    argb = bit.bor(argb, bit.lshift(r, 16)) -- r
+    argb = bit.bor(argb, bit.lshift(a, 24)) -- a
+    return argb
+  end
+  
+  function explode_argb(argb)
+    local a = bit.band(bit.rshift(argb, 24), 0xFF)
+    local r = bit.band(bit.rshift(argb, 16), 0xFF)
+    local g = bit.band(bit.rshift(argb, 8), 0xFF)
+    local b = bit.band(argb, 0xFF)
+    return a, r, g, b
+  end
